a885af6c06ac4513b626a1cb79e4cee44b6a96e6
LiangXiaoshun <hatsuroku@foxmail.com>
2023-03-03 13:46:21 +0800

    finish feat-b

---
+++ src/AdaptiveFrame.tsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+import GameIntroduction from './component/GameIntroduction';
+
+const Frame = styled.div`
+    margin: 48px;
+
+    display: flex;
+    flex-wrap: wrap;
+    gap: 24px;
+`;
+
+const AdaptiveFrame = () => <Frame>
+    <GameIntroduction></GameIntroduction>
+    <GameIntroduction></GameIntroduction>
+    <GameIntroduction></GameIntroduction>
+    <GameIntroduction></GameIntroduction>
+    <GameIntroduction></GameIntroduction>
+    <GameIntroduction></GameIntroduction>
+    <GameIntroduction></GameIntroduction>
+    <GameIntroduction></GameIntroduction>
+    <GameIntroduction></GameIntroduction>
+    <GameIntroduction></GameIntroduction>
+    <GameIntroduction></GameIntroduction>
+    <GameIntroduction></GameIntroduction>
+    <GameIntroduction></GameIntroduction>
+    <GameIntroduction></GameIntroduction>
+    <GameIntroduction></GameIntroduction>
+    <GameIntroduction></GameIntroduction>
+    <GameIntroduction></GameIntroduction>
+    <GameIntroduction></GameIntroduction>
+    <GameIntroduction></GameIntroduction>
+    <GameIntroduction></GameIntroduction>
+    <GameIntroduction></GameIntroduction>
+    <GameIntroduction></GameIntroduction>
+    <GameIntroduction></GameIntroduction>
+    <GameIntroduction></GameIntroduction>
+    <GameIntroduction></GameIntroduction>
+</Frame>
+
+export default AdaptiveFrame;
